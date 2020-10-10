39cf191e675a5898e38156635af3a48451b6d0a1
Mori 30R <ghahreman00@gmail.com>
2020-10-10 18:01:47 +0330

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-      <p>hello world</p>
+      <p>Hello World</p>
   </React.StrictMode>,
   document.getElementById('root')
 );
