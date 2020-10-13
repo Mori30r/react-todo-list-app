46cb984fd63a31b90c977fee3a970c38f305d93b
Mori 30R <ghahreman00@gmail.com>
2020-10-13 22:08:00 +0330

    unique key for every todo

--- src/js/components/DashboardPage/TodoList.js
+++ src/js/components/DashboardPage/TodoList.js
@@ -1,17 +1,17 @@
 import {TodoListItem} from "./TodoListItem";
 import {TodoContext} from "../../context/TodoContext";
 import React, {useContext} from "react";
+import { v4 as uuid } from 'uuid';
 
 
 export const TodoList = () => {
     const  { todos } = useContext(TodoContext);
-    console.log(todos)
     return (
         <div className="section__todos__list">
             {
                 todos.length > 0
                     ?
-                    todos.map((todo)=>{return <TodoListItem todo={todo}/>})
+                    todos.map((todo)=>{return <TodoListItem key={uuid()} todo={todo}/>})
                     :
                     <p className="section__todos__list--empty">Add Todo To Show In This Page</p>
             }
